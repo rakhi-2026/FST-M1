eb592d9f095097194274ac703fd3d66f6f16875c
rakhi-2026 <rakhdas1@in.ibm.com>
2026-06-18 10:49:39 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,4 @@
+user_name = input("Enter your name : ")
+user_age = int(input("Enter your age : "))
+year_of_100 = (2026 - user_age)+100
+print(f"{user_name} will be 100 year age in the year of {year_of_100}")
