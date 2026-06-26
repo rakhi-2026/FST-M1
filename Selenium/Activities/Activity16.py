52f6e60e6a97effc064675b55abed34cd86c988f
rakhi-2026 <rakhdas1@in.ibm.com>
2026-06-26 15:47:30 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity16.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.select import Select
+
+with webdriver.Firefox() as driver:
+    driver.get("https://training-support.net/webelements/selects")
+    print(f"The page title is : {driver.title}")
+
+    singleSelect = driver.find_element(By.CSS_SELECTOR,"select.h-10")
+    select = Select(singleSelect)
+    select.select_by_visible_text("Two")
+    select.select_by_index(2)
+    select.select_by_value("four")
+			
+    options = select.options
+    for  opt in options :
+       print(opt.text)
+    driver.quit();
